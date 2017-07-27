b748f03eae17ca3b17d2d30990f5f78902ea24e7
cn-src <public@javaer.cn>
2017-07-27 16:06:35 +0800

    add updateDemoPom.groovy 用于更新demo的pom.xml里的依赖
    Signed-off-by: zhangpeng <java@javaer.cn>

---
+++ updateDemoPom.groovy
@@ -0,0 +1,10 @@
+import groovy.xml.*
+
+def pom = new XmlSlurper(false, false).parse("./pom.xml")
+def demoPom = new XmlSlurper(false, false).parse("./demo/pom.xml")
+def dependencyList = pom.dependencyManagement.dependencies.children()
+dependencyList.version.replaceNode {}
+dependencyList.exclusions.replaceNode {}
+demoPom.dependencies = dependencyList
+
+XmlUtil.serialize(demoPom, new PrintWriter(new File("./demo/pom.xml")))
